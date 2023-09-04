29a4e682bf9c18d8cdbdcf8c3cc3342921032d5f
Stephane Reinstein <steph@sntial.co.za>
2023-09-04 09:07:38 +0200

    Update bigin.js
    PMS link alter

--- js/bigin.js
+++ js/bigin.js
@@ -10,4 +10,33 @@ jQuery(document).ready(function($) {
     var fieldContainer = inputField.closest('.wf-field');
     fieldContainer.hide();
     fieldContainer.prev('.wf-label').hide();
+
+
+    // alter PMS url
+    document.addEventListener("DOMContentLoaded", function() {
+        // Check if the user is on a mobile device
+        function isMobile() {
+            return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent);
+        }
+    
+        if (isMobile()) {
+            // Get all anchor elements (links) in the document
+            let links = document.querySelectorAll('a');
+    
+            links.forEach(link => {
+                let currentUrl = link.href;
+    
+                // Check if the URL has the structure you mentioned
+                if (/https:\/\/apps.hti-systems.com\/extraordinary\/[a-zA-Z0-9]+\/desktop.html/.test(currentUrl)) {
+                    // Replace 'desktop' with 'mobile'
+                    let mobileUrl = currentUrl.replace('/desktop.html', '/mobile.html');
+                    
+                    // Update the link's href
+                    link.href = mobileUrl;
+                }
+            });
+        }
+    });
+    
+
 });
